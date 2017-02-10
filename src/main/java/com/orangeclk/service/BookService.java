755fcad75d2d1afb90c8d096f0f33cac2fb83619
orangeclk <orangeclk@gmail.com>
2017-02-10 16:17:23 +0800

    v0.0.5 添加导航栏

--- src/main/java/com/orangeclk/service/BookService.java
+++ src/main/java/com/orangeclk/service/BookService.java
@@ -3,10 +3,13 @@ package com.orangeclk.service;
 import com.orangeclk.model.NewBook;
 import com.orangeclk.model.entity.BookEntity;
 
+import java.util.Set;
+
 /**
  * Created by orangeclk on 12/24/16.
  */
 public interface BookService {
     BookEntity findByIsbn(String isbn);
     BookEntity save(NewBook newBook);
+    Iterable<BookEntity> findAll();
 }
